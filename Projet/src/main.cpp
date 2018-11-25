1eca821e1b28fe55d94a5a52af94e7791fa1523c
Kevin XU <kevin-xu@hotmail.fr>
2018-11-25 20:13:58 +0100

    Création du projet PAP

---
+++ Projet/src/main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Projet_PAP.cpp
+// Author      : Kevin XU & Ziheng LI
+// Version     :
+// Copyright   : Your copyright notice
+// Description : PAP Project - Courbes de Bézier et police de caractères
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
