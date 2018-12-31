eb9a1838aa758bc3827d5cccda0b1382dce7dadd
Kevin XU <kevin-xu@hotmail.fr>
2018-12-31 10:18:56 +0100

    Ajout beziercurve.cpp

---
+++ test_libpng/testsyntax.c
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+	std::cout << int(1.8) << std::endl;
+	return 0;
+}
---
+++ test_libpng/testsyntax.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+	std::cout << round(1.4) << std::endl;
+	return 0;
+}
